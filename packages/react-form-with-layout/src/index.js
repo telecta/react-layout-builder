bc4a93b168537a64ba8017c8d7466877e8465a3e
alvinsj <text@alvinsj.net>
2017-08-17 15:03:08 +0300

    prettier and satisfy eslint

--- packages/react-form-with-layout/src/index.js
+++ packages/react-form-with-layout/src/index.js
@@ -4,122 +4,127 @@ import invariant from 'invariant';
 
 import humanize from './utils/humanize';
 import assign from 'object-assign';
-import {inputValueLookup} from 'form-input-serialize';
-import {layout, section, col} from 'react-layout-builder';
+import { inputValueLookup } from 'form-input-serialize';
+import { layout, section, col } from 'react-layout-builder';
 
 export default class FormWithLayout extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-        invariant(props.renderLayout && props.renderField,
-            'props.renderLayout and props.renderField is required.');
+    invariant(
+      props.renderLayout && props.renderField,
+      'props.renderLayout and props.renderField is required.'
+    );
 
-        this._refs = {};
-        this._isMounted = false;
+    this._refs = {};
+    this._isMounted = false;
 
-        this.renderField = this.renderField.bind(this);
-        this.fields = {};
-    }
+    this.renderField = this.renderField.bind(this);
+    this.fields = {};
+  }
 
-    componentDidMount () {
-        this._isMounted = true;
-    }
+  componentDidMount() {
+    this._isMounted = true;
+  }
 
-    render () {
-        const {props} = this;
+  render() {
+    const { props } = this;
 
-        const formProps = {
-            action: props.action,
-            method: props.method,
-            onSubmit: props.onSubmit
-        };
+    const formProps = {
+      action: props.action,
+      method: props.method,
+      onSubmit: props.onSubmit
+    };
 
-        const formBuilder = {layout: layout, section: section};
-        formBuilder.col = col.bind(null, this.renderField);
+    const formBuilder = { layout: layout, section: section };
+    formBuilder.col = col.bind(null, this.renderField);
 
-        const children = this.props.showAll && props.renderExpandedLayout ?
-            props.renderExpandedLayout(formBuilder, props)
-            : props.renderLayout(formBuilder, props);
+    const children =
+      this.props.showAll && props.renderExpandedLayout
+        ? props.renderExpandedLayout(formBuilder, props)
+        : props.renderLayout(formBuilder, props);
 
-        return (
-            <form ref={c => this.form = c}
-                {...formProps}
+    return (
+      <form
+        ref={c => (this.form = c)}
+        {...formProps}
+        className={props.className || 'form-horizontal'}
+      >
+        {children}
+        {props.renderButtons ? props.renderButtons(props) : <div />}
+      </form>
+    );
+  }
 
-                className={props.className || 'form-horizontal'}>
+  renderField(name) {
+    const props = this.props;
 
-                {children}
-                {props.renderButtons ? props.renderButtons(props) : <div />}
-            </form>);
-    }
+    const value = inputValueLookup(props.values || {}, name);
+    const error = props.errors ? props.errors[name] : null;
+    const def = props.getFieldProps(name);
 
-    renderField (name) {
-        const props = this.props;
+    // form.defaultValues(field).defaultValue
+    var defaultValue = inputValueLookup(props.defaultValues || {}, name);
 
-        const value = inputValueLookup(props.values || {}, name);
-        const error = props.errors ? props.errors[name] : null;
-        const def = props.getFieldProps(name);
+    // fieldProps.value > form.defaultValues(field).defaultValue
+    defaultValue =
+      def.defaultValue === null || typeof def.defaultValue === 'undefined'
+        ? defaultValue
+        : def.defaultValue;
 
-        // form.defaultValues(field).defaultValue
-        var defaultValue = inputValueLookup(props.defaultValues || {}, name);
+    // form.props.disabled > fieldProps.disabled
+    var disabled = props.disabled || def.disabled;
 
-        // fieldProps.value > form.defaultValues(field).defaultValue
-        defaultValue = def.defaultValue === null
-            || typeof def.defaultValue === 'undefined'
-              ? defaultValue
-              : def.defaultValue;
+    var label = def.label ? def.label : humanize(name);
 
-        // form.props.disabled > fieldProps.disabled
-        var disabled = props.disabled || def.disabled;
+    var refName = this.getFieldRef(name);
 
-        var label = def.label ? def.label : humanize(name);
+    const fieldProps = assign({}, def, {
+      ref: c => {
+        this.fields[refName] = c;
+      },
+      // key: refName,
+      className: 'field',
+      field: def,
+      name: name,
+      value: value,
+      defaultValue: defaultValue,
+      label: label,
+      error: error,
+      disabled: disabled
+    });
 
-        var refName = this.getFieldRef(name);
+    return def.input
+      ? React.createElement(def.input, fieldProps)
+      : props.renderField(name, fieldProps);
+  }
 
-        const fieldProps = assign({}, def, {
-            ref: c => {this.fields[refName] = c;},
-            // key: refName,
-            className: 'field',
-            field: def,
-            name: name,
-            value: value,
-            defaultValue: defaultValue,
-            label: label,
-            error: error,
-            disabled: disabled
-        });
+  /** contextual features */
+  getFieldRef(name) {
+    var refName = 'field-' + name;
+    var count = this._refs[refName];
 
-        return def.input ?
-            React.createElement(def.input, fieldProps)
-            : props.renderField(name, fieldProps);
-    }
+    if (!this._isMounted) this._refs[refName] = count ? count + 1 : 1;
 
-    /** contextual features */
-    getFieldRef (name) {
-        var refName = 'field-'+name;
-        var count = this._refs[refName];
-
-        if(!this._isMounted)
-            this._refs[refName] = count ? (count+1) : 1;
-
-        count = this._refs[refName];
-        refName = refName + (count == 1 ? '' : '-'+ (this._refs[refName]-1));
-        return refName;
-    }
+    count = this._refs[refName];
+    refName = refName + (count === 1 ? '' : '-' + (this._refs[refName] - 1));
+    return refName;
+  }
 }
 
 FormWithLayout.propTypes = {
-    renderLayout: PropTypes.func.isRequired,
-    renderField: PropTypes.func.isRequired,
-    getFieldProps: PropTypes.func.isRequired,
+  renderLayout: PropTypes.func.isRequired,
+  renderField: PropTypes.func.isRequired,
+  getFieldProps: PropTypes.func.isRequired,
 
-    values: PropTypes.object,
-    defaultValues: PropTypes.object,
+  values: PropTypes.object,
+  defaultValues: PropTypes.object,
 
-    errors: PropTypes.object,
-    disabled: PropTypes.bool,
-    className: PropTypes.string,
+  errors: PropTypes.object,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
 
-    renderButtons: PropTypes.func,
-    renderExpandedLayout: PropTypes.func,
-    showAll: PropTypes.bool
+  renderButtons: PropTypes.func,
+  renderExpandedLayout: PropTypes.func,
+  showAll: PropTypes.bool
 };
--- packages/react-layout-builder/src/index.js
+++ packages/react-layout-builder/src/index.js
@@ -2,31 +2,42 @@ import React from 'react';
 
 var layoutCount = 0;
 export const layout = (...children) => {
-    return <div key={`layout-${layoutCount++}`} className="layout">{children}</div>;
+  return (
+    <div key={`layout-${layoutCount++}`} className="layout">
+      {children}
+    </div>
+  );
 };
 
 var sectionCount = 0;
 export const section = (name, ...rows) => {
-    const renderedRows = rows.map((cols, index) => {
-        return <div key={`columns-${index}`} className="columns">{cols}</div>;
-    });
+  const renderedRows = rows.map((cols, index) => {
     return (
-        <div key={`section-${sectionCount++}`} className="section">
-            {name && name !== ''? <h5>{name}</h5> : <div />}
-            {renderedRows}
-        </div>);
+      <div key={`columns-${index}`} className="columns">
+        {cols}
+      </div>
+    );
+  });
+  return (
+    <div key={`section-${sectionCount++}`} className="section">
+      {name && name !== ''
+        ? <h5>
+            {name}
+          </h5>
+        : <div />}
+      {renderedRows}
+    </div>
+  );
 };
 
 export const col = (renderField, type, ...fields) => {
-    if(fields.length == 0)
-        return <div className={`column ${type}`} />;
+  if (fields.length === 0) return <div className={`column ${type}`} />;
 
-    return fields.map((field, index) => {
-
-        return (
-        <div key={`col-${index}`}
-            className={`column ${type}`}>
-            {typeof field === 'string' ? renderField(field) : field}
-        </div>);
-    });
+  return fields.map((field, index) => {
+    return (
+      <div key={`col-${index}`} className={`column ${type}`}>
+        {typeof field === 'string' ? renderField(field) : field}
+      </div>
+    );
+  });
 };
