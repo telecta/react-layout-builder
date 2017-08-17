eeaa3c809af3b18c09ad2efd06d7025a278f1a50
alvinsj <text@alvinsj.net>
2017-08-17 17:57:18 +0300

    fix tests

--- packages/react-form-with-layout/__tests__/index-test.js
+++ packages/react-form-with-layout/__tests__/index-test.js
@@ -43,7 +43,7 @@ FormInput.propTypes = {
   name: PropTypes.string,
   type: PropTypes.string,
   className: PropTypes.string,
-  defaultValue: PropTypes.string
+  defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 };
 
 describe('FormWithLayout', () => {
@@ -135,21 +135,21 @@ describe('FormWithLayout', () => {
       onSubmit: PropTypes.func
     };
 
-    componentProps = { showAll: true, defaultValues: {} };
+    componentProps = { showAll: true, defaultValues: { age: 0 } };
     component = mount(<FormOwner {...componentProps} />);
   });
 
   describe('#create', () => {
-    it('should generate form', () => {
+    it('renders form', () => {
       expect(component.find('form').length).toBe(1);
     });
 
-    it('should duplicate field refs if two same fields', () => {
+    it('renders two same fields', () => {
       const form = component.find('form');
       expect(form.find('[name="name"]').length).toBe(2);
     });
 
-    it('should not duplicate field refs when updated', () => {
+    it('renders same number of fields when updated', () => {
       const form = component.find('form');
       expect(form.find('.field').length).toBe(10);
 
@@ -157,55 +157,67 @@ describe('FormWithLayout', () => {
       expect(form.find('.field').length).toBe(10);
     });
 
-    it('should generate 1 layout and 2 sections', () => {
+    it('renders 1 layout and 2 sections', () => {
       expect(component.find('.layout').length).toBe(1);
-
       expect(component.find('.section').length).toBe(2);
     });
 
-    it('should generate inputs', () => {
-      var search = component.find(FormInput);
-      expect(search.length).toBe(10);
+    describe('renders inputs', () => {
+      var name, type, value;
+      beforeAll(() => {
+        var search = component.find(FormInput);
+        expect(search.length).toBe(10);
 
-      search = component.find('input');
-      expect(search.length).toBe(12);
+        search = component.find('input');
+        expect(search.length).toBe(12);
 
-      const name = n => {
-        return search.at(n).node.getAttribute('name');
-      };
-      const type = n => {
-        return search.at(n).node.getAttribute('type');
-      };
-      const value = n => {
-        return search.at(n).node.value;
-      };
+        name = n => {
+          return search.at(n).node.getAttribute('name');
+        };
+        type = n => {
+          return search.at(n).node.getAttribute('type');
+        };
+        value = n => {
+          return search.at(n).node.value;
+        };
+      });
 
-      expect(name(0)).toBe('name');
-      expect(name(1)).toBe('age');
+      it('renders all inputs', () => {
+        expect(name(0)).toBe('name');
+        expect(name(1)).toBe('age');
+        expect(name(2)).toBe('email');
+        expect(name(3)).toBe('address[0]');
+        expect(name(4)).toBe('address[1]');
+        expect(name(5)).toBe('role');
+        expect(name(6)).toBe('name');
+        expect(name(7)).toBe('age');
+        expect(name(8)).toBe('email');
+        expect(name(9)).toBe('address[0]');
+        expect(name(10)).toBe('address[1]');
+        expect(name(11)).toBe('role');
+        expect(value(11)).toBe('wife');
+      });
 
-      expect(name(2)).toBe('email');
-      expect(type(2)).toBe('email');
+      it('renders input with correct type', () => {
+        expect(name(2)).toBe('email');
+        expect(type(2)).toBe('email');
+      });
 
-      expect(name(3)).toBe('address[0]');
-      expect(name(4)).toBe('address[1]');
+      it('renders nodes with value', () => {
+        expect(name(5)).toBe('role');
+        expect(value(5)).toBe('husband');
 
-      expect(name(5)).toBe('role');
-      expect(value(5)).toBe('husband');
+        expect(name(11)).toBe('role');
+        expect(value(11)).toBe('wife');
+      });
 
-      expect(name(6)).toBe('name');
-      expect(name(7)).toBe('age');
-
-      expect(name(8)).toBe('email');
-      expect(type(8)).toBe('email');
-
-      expect(name(9)).toBe('address[0]');
-      expect(name(10)).toBe('address[1]');
-
-      expect(name(11)).toBe('role');
-      expect(value(11)).toBe('wife');
+      it('renders input with 0 value', () => {
+        expect(name(7)).toBe('age');
+        expect(value(7)).toBe('0');
+      });
     });
 
-    it('should generate lesser inputs when showAll=false', () => {
+    it('renders lesser inputs when showAll=false', () => {
       component.setProps({ showAll: false });
 
       expect(component.props().showAll).toBe(false);
@@ -216,12 +228,12 @@ describe('FormWithLayout', () => {
       expect(search.length).toBe(3);
     });
 
-    it('should submit the form', () => {
+    it('submit the form', () => {
       component.find('form').simulate('submit');
       expect(createActions).toBeCalled();
     });
 
-    it('should submit the form with values', () => {
+    it('submit the form with values', () => {
       const name = 'garfield';
       component.setProps({ showAll: false, defaultValues: { name: name } });
 
@@ -232,7 +244,7 @@ describe('FormWithLayout', () => {
       });
     });
 
-    it('should have form context/this in onSubmit method', () => {
+    it('passes the form node in onSubmit', () => {
       const grabContext = jest.fn();
       const onSubmit = jest.fn(function(e) {
         grabContext(e.target);
